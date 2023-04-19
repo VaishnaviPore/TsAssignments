8c6a93c2e3da44c87d24ac3360e8fac6c4ae9083
Vaishnavi Bakare <vaishnavibakare4@gmail.com>
2023-04-19 17:46:20 +0530

    Assignment 10 Routing adding

---
+++ assignment10/src_assignment10_Q3/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { TechnologyComponent } from './technology/technology.component';
+import { BooksComponent } from './books/books.component';
+import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
+
+const routes: Routes = [
+  {path:'',component:TechnologyComponent},
+  {path:'books',component:BooksComponent},
+  {path:'**',component:PagenotfoundComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
